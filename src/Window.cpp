cfdf620bae30b6ee3db77dce7614e64a3a931528
francisco melazzini <francisco.melazzini@physics.msu.ru>
2022-06-28 11:24:36 +0300

    more minor refactoring

--- src/Window.cpp
+++ src/Window.cpp
@@ -1,7 +1,1 @@
 #include "Window.hpp"
-
-const Color Color::Black = {0, 0, 0, 255};
-const Color Color::White = {255, 255, 255, 255};
-const Color Color::Blue = {0, 0, 255, 255};
-const Color Color::Red = {255, 0, 0, 255};
-const Color Color::Green = {0, 255, 0, 255};
--- src/utils.cpp
+++ src/utils.cpp
@@ -3,6 +3,12 @@
 
 #include "utils.hpp"
 
+const Color Color::Black = {0, 0, 0, 255};
+const Color Color::White = {255, 255, 255, 255};
+const Color Color::Blue = {0, 0, 255, 255};
+const Color Color::Red = {255, 0, 0, 255};
+const Color Color::Green = {0, 255, 0, 255};
+
 auto renderer_creator = [](SDL_Window *wnd)
 {
     auto r = SDL_CreateRenderer(wnd,
