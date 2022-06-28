18733082c624b9007b7d5804b21e861ca39a64e8
francisco melazzini <francisco.melazzini@physics.msu.ru>
2022-06-28 11:22:28 +0300

    minor refactoring

---
+++ test/WindowTest.cpp
@@ -0,0 +1,15 @@
+#include <gmock/gmock.h>
+#include "Window.hpp"
+#include "ImageLoader.hpp"
+#include "Renderer.hpp"
+
+using testing::_;
+using testing::Eq;
+using testing::Invoke;
+using testing::NiceMock;
+using testing::Return;
+using testing::Test;
+
+TEST(A,B)
+{
+}
